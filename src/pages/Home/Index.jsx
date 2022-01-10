1396a1189162d8c04d236bdf6a178ad5e7e60086
Cristian Sanchez <ederson12f@gmail.com>
2022-01-10 14:59:09 -0500

    parallax effect in the hero wrapper of the home page

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Header } from '../../components/Header/Index'
-import HeroWrapper from '../../components/HeroWrapper'
+import { Header } from '@components/Header/Index'
+import { HeroWrapper } from '@components/HeroWrapper'
 
 export const Home = () => {
   return (
